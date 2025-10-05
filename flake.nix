75e56c5a1086baee7a1b59ac3c107b9804bf9995
rwebster <34316509+rgdubs@users.noreply.github.com>
2025-10-05 14:30:10 -0500

    bringing it to a stable branch until i figure out why it's not working properly on my setup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for installing NordVPN on NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }:
